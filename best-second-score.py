ff9b40e2a9be75c3d75daf02667a0fb21217f94e
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2022-10-30 02:12:52 -0500

    ex 3 solved

---
+++ best-second-score.py
@@ -0,0 +1,19 @@
+import math
+def firstSecond(givenList):
+    # TODO
+    max1 = -math.inf
+    for i in range(0,len(givenList)):
+        # for j in range(1,len(givenList))
+        # max1 = givenList[i-1]
+        # max2 = givenList[i-1]
+
+        # max2 = 
+        if givenList[i] > max1:
+            max2 = max1
+            max1 = givenList[i]
+            
+        return max1,max2
+
+        
+myList = [84,85,86,87,85,90,85,83,23,45,84,1,2,0]
+firstSecond(myList) # 90 87
